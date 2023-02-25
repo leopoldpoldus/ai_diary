ecc260a8cbccab8211b8f87419acbef37a4b3830
Leopold Wieser <leopoldwieser1@gmail.com>
2023-02-25 22:30:50 +0100

    initial commit

---
+++ ai_diary/README.md
@@ -0,0 +1,15 @@
+# AI Diary
+
+This is a README file for the AI Diary project.
+
+## Introduction
+
+The AI Diary is a project that should give you the ability to convert speech to diary entries.
+
+## Installation
+
+
+
+### Requirements
+
+
---
+++ ai_diary/src/App.jsx
@@ -0,0 +1,35 @@
+import {useState, useEffect} from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import generatePrompts from './components/GPT3_API.jsx';
+import TranscriptionModule from "./components/TranscriptionModule.jsx";
+
+const App = () => {
+    const [prompt, setPrompt] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setPrompt(event.target.elements.prompt.value);
+    };
+
+    useEffect(() => {
+        if (prompt) {
+            generatePrompts(prompt).then(completion => setAnswer(completion));
+        }
+    }, [prompt]);
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <textarea name="prompt"/>
+                <button type="submit">Submit</button>
+            </form>
+            {prompt && <div>{answer}</div>}
+            <TranscriptionModule/>
+        </div>
+    );
+};
+
+export default App;
+
